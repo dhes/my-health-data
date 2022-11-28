800042fafc066f7006bf8fb57c4b15405d13d19d
Dan Heslinga <dan@heslinga.us>
2022-11-28 13:53:48 -1000

    examine endpoints object

--- src/index.js
+++ src/index.js
@@ -33,14 +33,14 @@ async function makeClient(fhirBaseUrl, steps = ['oauth', 'authorize', 'token']) 
   }
   return clientState
 }
-console.log("endpoints[1]: " + endpoints[1]);
+console.log("endpoints[1]: ", endpoints[1]);
 console.log("endpoints[1].fhirBaseUrl: " + endpoints[1].fhirBaseUrl);
 // Eventually we'll have a UI
 let serverPick = window.location.hash.slice(1) || 'epic'
 let fhirServerToTest = {
   smart: endpoints[0].fhirBaseUrl, // smart sandbox
   // epic: endpoints[1].fhirBaseUrl, //older argonaut epic sandbox endpoint
-  epic: endpoints[2].fhirBaseUrl, //newer epic test endpoint added by dan
+  epic: endpoints[2].fhirBaseUrl, //newer epic sandbox endpoint added by dan
   unity: 'https://epicfhir.unitypoint.org/ProdFHIR/api/FHIR/DSTU2/',
   uw: 'https://epicproxy.hosp.wisc.edu/FhirProxy/api/FHIR/DSTU2/',
 	queens: 'https://mobileapps.queens.org/FHIR/api/FHIR/R4/'
