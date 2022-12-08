9267355702cb8483104691b50307bf160e3cc04a
Dan Heslinga <dan@heslinga.us>
2022-12-08 11:04:57 -1000

    new directory for scripts; commti reduceEntries.js

---
+++ scripts/reduceEntries.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+const bundles = require("./me/queensv2.json"); // where each 'bundle' is a array with one entry whose value is a Bundle resource
+let bundleEntrys = []; // Entry stands for FHIR Budle entry array
+bundles.forEach((bundle) => bundleEntrys.push(bundle[0].entry)); // each 'bundleEntry is an array with one Bundle resourse
+let allBundleEntries = [];
+// this has the same effect as flattening..
+// allBundleEntries = bundleEntrys.reduce((accumulator, currentValue) =>
+//   accumulator.concat(...currentValue)
+// );
+allBundleEntries = bundleEntrys.flat(1);
+// console.log(JSON.stringify(allBundleEntries).normalize() === JSON.stringify(flattenedBundleEntries).normalize()); // false
+// fs.writeFile(
+//   "./me/allBundleEntries.json",
+//   JSON.stringify(allBundleEntries),
+//   (err) => {
+//     if (err) throw err;
+//   }
+// );
+fs.writeFile(
+  "./me/allBundleEntries.json",
+  JSON.stringify(allBundleEntries),
+  (err) => {
+    if (err) throw err;
+  }
+);
+// filter out all OperationOutcome resources
+constfilteredEntries = allBundleEntries.filter((value) => value.resource.resourceType != "OperationOutcome");
+fs.writeFile(
+  "./me/filteredEntries.json",
+  JSON.stringify(filteredEntries),
+  (err) => {
+    if (err) throw err;
+  }
+);
+
