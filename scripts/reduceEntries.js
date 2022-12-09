504591dbd5b7b01e1e69c988e039bf4b4d68483f
Dan Heslinga <dan@heslinga.us>
2022-12-08 17:09:42 -1000

    write out filtered record automatically

--- scripts/reduceEntries.js
+++ scripts/reduceEntries.js
@@ -1,23 +1,29 @@
 const fs = require("fs");
-const bundles = require("./me/queensv2.json"); // where each 'bundle' is a array with one entry whose value is a Bundle resource
+const sourceFile = "../me/" + process.argv[2] + ".json";
+const destFile = process.cwd() + "/me/" + process.argv[3] + ".json";
+const filteredFile = process.cwd() + "/me/" + process.argv[3] + "Filtered.json";
+console.log(sourceFile + " " + destFile);
+const bundles = require(sourceFile); // where each 'bundle' is a array with one entry whose value is a Bundle resource
+// console.log();
 let bundleEntrys = []; // Entry stands for FHIR Bundle entry element, confusing because entry has many items
 bundles.forEach((bundle) => bundleEntrys.push(bundle[0].entry)); // each 'bundleEntry is an array with one Bundle resource
 let allBundleEntries = [];
 allBundleEntries = bundleEntrys.flat(1);
 fs.writeFile(
-  "./me/allQueensResources.json",
+  destFile,
   JSON.stringify(allBundleEntries),
   (err) => {
     if (err) throw err;
   }
 );
 // filter out all OperationOutcome resources
-const filteredEntries = allBundleEntries.filter((value) => value.resource.resourceType != "OperationOutcome");
+const filteredEntries = allBundleEntries.filter(
+  (value) => value.resource.resourceType != "OperationOutcome"
+);
 fs.writeFile(
-  "./me/filteredEntries.json",
+  filteredFile,
   JSON.stringify(filteredEntries),
   (err) => {
     if (err) throw err;
   }
 );
-
